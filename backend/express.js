73013de839a9f8788ce024798dc50be467026b07
mihaiwebdev <1mihaisava1@gmail.com>
2023-02-05 17:13:43 +0200

    Added express server for form submissions handling, and animations

---
+++ backend/express.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const dotenv = require('dotenv').config()
+const nodemailer = require('nodemailer')
+const app = express()
+const port = 5000
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.AUTH_USER,
+        pass: process.env.AUTH_PASS
+        
+    },
+})
+
+app.use(express.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.post('/contact', (req, res) => {
+    const name = req.body.name
+    const email = req.body.email
+    const message = req.body.text
+
+    const mailOptions = {
+        from: process.env.AUTH_USER,
+        to: process.env.RECEIVER,
+        subject: 'New Contact Form Submission',
+        text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
+    }
+
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+    
+            res.status(404).json({message: error})
+        
+        } else {
+            res.status(201).json({message: 'Message sent!'})
+        }
+
+    })
+
+})
+
+
+app.listen(port, () => {
+    console.log('Server started on port 5000')
+})
